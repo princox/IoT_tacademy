c56a627e5ddb32b957836684a75e9e0ded316a98
princox <rjhoon92@naver.com>
2016-08-22 15:49:09 +0900

    second example / temp and humidity

---
+++ dh11_example/dh11_example.ino
@@ -0,0 +1,25 @@
+#include "DHT11.h"
+DHT11 dht11(2);
+//input is pin 2
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+int error;
+float temp, humidity;
+if ((error = dht11.read(humidity, temp)) == 0) {
+  Serial.print("temp: ");
+  Serial.print(temp);
+  Serial.print("humidity:");
+  Serial.println(humidity);
+}
+}
+
+
+
+
